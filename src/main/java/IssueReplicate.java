84168b07a576b0e9aef93ec8348e7f3c903fc3bd
tariq.ibrahim <tariq.ibrahim@kohls.com>
2015-10-29 17:52:50 +0530

    Initial commit

---
+++ src/main/java/IssueReplicate.java
@@ -0,0 +1,21 @@
+import org.apache.maven.cli.MavenCli;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created by tariq.ibrahim on 29-10-2015.
+ */
+public class IssueReplicate {
+
+    public static void main(String args[]) {
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("-Dmaven.multiModuleProjectDirectory", projectPath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(baos);
+        MavenCli mavenCli = new MavenCli();
+        mavenCli.doMain(new String[] {"install"}, projectPath, out, out);
+        out.close();
+        System.out.print(baos.toString());
+    }
+}
