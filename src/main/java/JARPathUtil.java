b095f11e2bdf95e705fd5420fb04630acfb2eb4e
Tariq <tariq1890@yahoo.in>
2015-11-22 17:11:57 +0530

    Ensure OS Appropriate Paths

--- src/main/java/JARPathUtil.java
+++ src/main/java/JARPathUtil.java
@@ -6,17 +6,21 @@ import java.nio.file.Paths;
  */
 public final class JARPathUtil {
 
-    public  static String getJARPathString(final Class clazz) {
+    private static final boolean IS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
+
+    public static String getJARPathString(final Class clazz) {
         return getJARPath(clazz).toString();
 
     }
 
-    public  static String getJARFileName(final Class clazz) {
+    public static String getJARFileName(final Class clazz) {
         return getJARPath(clazz).getFileName().toString();
 
     }
 
     private static Path getJARPath(final Class clazz) {
-        return Paths.get(clazz.getProtectionDomain().getCodeSource().getLocation().getPath().substring(1));
+        final String pathString = clazz.getProtectionDomain().getCodeSource().getLocation().getPath();
+        final String osAppropriatePath = IS_WINDOWS ? pathString.substring(1) : pathString;
+        return Paths.get(osAppropriatePath);
     }
 }
